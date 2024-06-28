ac156e17f671f7e94cd7156afb80d7cfffa6d05d
Sukhpreet Singh <sukhpreetben10@gmail.com>
2024-06-28 22:02:30 +0530

    removed unused dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,15 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from "@astrojs/cloudflare";
 import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
-
 import mdx from "@astrojs/mdx";
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://sukhpreet.dev',
-  output: "server",
+  output: "hybrid",
   adapter: cloudflare({
-    runtime: {
-      mode: "local"
+    platformProxy: {
+      enabled: true
     }
   }),
-  integrations: [tailwind(),  react(), mdx()]
+  integrations: [tailwind(), mdx()]
 });

49239673cc565bce212f79a0fbd12a9ce77bdf8f
Sukhpreet Singh <sukhpreetben10@gmail.com>
2024-09-18 16:08:24 +0530

    quick fix to some type errors on build

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,4 +1,4 @@
-import { defineCollection, reference, z } from "astro:content";
+import { defineCollection, z } from "astro:content";
 import { glob } from "astro/loaders";
 
 const articles = defineCollection({

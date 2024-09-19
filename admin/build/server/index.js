1cc411b312f9ac276be35d38f83f033c08418b31
Sukhpreet Singh <sukhpreetben10@gmail.com>
2024-09-19 15:42:23 +0530

    added @remix-run/node package

--- admin/build/server/index.js
+++ admin/build/server/index.js
@@ -128,10 +128,20 @@ const route0 = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProper
   __proto__: null,
   default: App
 }, Symbol.toStringTag, { value: "Module" }));
+const storage = process.env.NODE_ENV === "development" ? { kind: "local" } : {
+  kind: "github",
+  repo: {
+    owner: "q1b",
+    name: "q1b"
+  }
+};
 const markdocConfig = fields.markdoc.createMarkdocConfig({});
 const keystaticConfig = config({
-  storage: {
-    kind: "local"
+  storage,
+  ui: {
+    brand: {
+      name: "Admin Dashboard"
+    }
   },
   singletons: {
     homepage: singleton({
@@ -162,17 +172,256 @@ const keystaticConfig = config({
           }
         )
       }
+    }),
+    person: singleton({
+      label: "Profile",
+      path: "src/content/singletons/profile/",
+      format: {
+        data: "json"
+      },
+      schema: {
+        name: fields.text({ label: "Name" }),
+        email_address: fields.text({
+          label: "Email Address",
+          description: "Be aware of the email address you are typing as this is going to be visible to everyone who is going to visit this website!"
+        }),
+        image: fields.image({
+          label: "Profile Image",
+          directory: "src/assets/images/profile",
+          publicPath: "/src/assets/images/profile/"
+        }),
+        worksFor: fields.conditional(
+          fields.checkbox({
+            label: "Are you Currently Working ?",
+            defaultValue: false
+          }),
+          {
+            true: fields.text({ label: "Works For" }),
+            false: fields.empty()
+          }
+        ),
+        knowsAbout: fields.array(
+          fields.object({
+            name: fields.text({ label: "Name" }),
+            public: fields.checkbox({
+              label: "Public",
+              defaultValue: true
+            })
+          }),
+          {
+            label: "Knows About",
+            itemLabel: (props) => props.fields.name.value
+          }
+        ),
+        githubURL: fields.url({ label: "GitHub URL" }),
+        xURL: fields.url({ label: "X URL" }),
+        linkedInURL: fields.url({ label: "LinkedIn URL" }),
+        resumePDF: fields.file({
+          label: "PDF File of your Resume",
+          directory: "src/assets/files/resume",
+          publicPath: "/src/assets/files/resume/"
+        })
+      }
     })
   },
   collections: {
-    posts: collection({
-      label: "Posts",
+    projects: collection({
+      label: "Projects",
       slugField: "title",
-      path: "app/content/posts/*",
+      path: "src/data/projects/*",
+      format: {
+        data: "json",
+        contentField: "content"
+      },
+      schema: {
+        title: fields.slug({ name: { label: "Title" } }),
+        shortDescription: fields.text({ label: "Short Description" }),
+        visible: fields.checkbox({
+          label: "Visible"
+        }),
+        isMajorProject: fields.checkbox({
+          label: "Is Major Project"
+        }),
+        githubURL: fields.url({ label: "GitHub URL" }),
+        websiteURL: fields.url({ label: "Website URL" }),
+        article: fields.conditional(
+          fields.checkbox({
+            label: "Is Article"
+          }),
+          {
+            true: fields.object(
+              {
+                employmentType: fields.select({
+                  label: "Employment Type",
+                  options: [
+                    {
+                      label: "Full Time",
+                      value: "full-time"
+                    },
+                    {
+                      label: "Part Time",
+                      value: "part-time"
+                    },
+                    {
+                      label: "Self Employed",
+                      value: "self-employed"
+                    },
+                    {
+                      label: "Freelance",
+                      value: "freelance"
+                    },
+                    {
+                      label: "Internship",
+                      value: "internship"
+                    },
+                    {
+                      label: "Trainee",
+                      value: "trainee"
+                    }
+                  ],
+                  defaultValue: "full-time"
+                }),
+                location: fields.text({ label: "Location" }),
+                locationType: fields.select({
+                  label: "Location Type",
+                  options: [
+                    {
+                      label: "On-site",
+                      value: "on-site"
+                    },
+                    {
+                      label: "Hybrid",
+                      value: "hybrid"
+                    },
+                    {
+                      label: "Remote",
+                      value: "remote"
+                    }
+                  ],
+                  defaultValue: "remote"
+                }),
+                startDate: fields.date({ label: "Start Date" }),
+                endDate: fields.date({ label: "End Date" }),
+                thumbnail: fields.object({
+                  "16/9": fields.image({
+                    label: "16/9 Aspect Ratio Image",
+                    directory: "src/assets/images/projects",
+                    // Use the @assets path alias
+                    // publicPath: '@assets/images/articles/'
+                    publicPath: "/src/assets/images/projects/"
+                  }),
+                  "1/1": fields.image({
+                    label: "1/1 Aspect Ratio Image",
+                    directory: "src/assets/images/projects",
+                    // Use the @assets path alias
+                    // publicPath: '@assets/images/articles/'
+                    publicPath: "/src/assets/images/projects/"
+                  })
+                }),
+                media: fields.array(
+                  fields.object({
+                    name: fields.text({ label: "Name" }),
+                    file: fields.file({
+                      label: "Media",
+                      directory: "src/assets/images/projects",
+                      // Use the @assets path alias
+                      // publicPath: '@assets/images/articles/'
+                      publicPath: "/src/assets/images/projects/"
+                    })
+                  }),
+                  {
+                    label: "Media",
+                    description: "Letter of Recommendation, Certificates, etc."
+                  }
+                ),
+                company: fields.object(
+                  {
+                    name: fields.text({ label: "Name" }),
+                    url: fields.url({ label: "URL" })
+                  },
+                  {
+                    label: "Company",
+                    description: "Details of the Company you worked for."
+                  }
+                )
+              },
+              {
+                label: "Article"
+              }
+            ),
+            false: fields.ignored()
+          }
+        ),
+        content: fields.markdoc({ label: "Content" })
+      }
+    }),
+    categories: collection({
+      label: "Categories",
+      slugField: "name",
+      path: "src/data/categories/*",
+      format: {
+        data: "json"
+      },
+      schema: {
+        name: fields.slug({ name: { label: "Name" } })
+      }
+    }),
+    tags: collection({
+      label: "Tags",
+      slugField: "name",
+      path: "src/data/tags/*",
+      format: {
+        data: "json"
+      },
+      schema: {
+        name: fields.slug({ name: { label: "Name" } }),
+        category: fields.relationship({
+          label: "Category",
+          collection: "categories"
+        })
+      }
+    }),
+    articles: collection({
+      label: "Articles",
+      slugField: "title",
+      path: "src/data/articles/*",
+      entryLayout: "content",
       format: { contentField: "content" },
       schema: {
         title: fields.slug({ name: { label: "Title" } }),
-        content: fields.markdoc({ label: "Content" })
+        subtitle: fields.text({ label: "Subtitle", multiline: true }),
+        summary: fields.text({ label: "Summary", multiline: true }),
+        content: fields.markdoc({ label: "Content" }),
+        visible: fields.checkbox({
+          label: "Visible",
+          defaultValue: false
+        }),
+        pubDate: fields.date({ label: "Pub Date" }),
+        thumbnail: fields.image({
+          label: "Thumbnail",
+          directory: "src/assets/images/articles",
+          // Use the @assets path alias
+          // publicPath: '@assets/images/articles/'
+          publicPath: "/src/assets/images/articles/"
+        }),
+        tags: fields.multiRelationship({
+          label: "Tags",
+          collection: "tags"
+        })
+      }
+    }),
+    webpages: collection({
+      label: "Webpages",
+      slugField: "path",
+      path: "src/data/webpages/*",
+      schema: {
+        path: fields.slug({ name: { label: "Path" } }),
+        title: fields.text({ label: "Title" }),
+        description: fields.text({
+          label: "Description",
+          multiline: true
+        }),
+        keywords: fields.array(fields.text({ label: "Keywords" }))
       }
     })
   }
@@ -256,7 +505,7 @@ const route4 = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProper
   loader,
   meta
 }, Symbol.toStringTag, { value: "Module" }));
-const serverManifest = { "entry": { "module": "/assets/entry.client-CfmtkiP9.js", "imports": ["/assets/index-DFWxKLZT.js", "/assets/components-DuNu-YZ6.js"], "css": [] }, "routes": { "root": { "id": "root", "parentId": void 0, "path": "", "index": void 0, "caseSensitive": void 0, "hasAction": false, "hasLoader": false, "hasClientAction": false, "hasClientLoader": false, "hasErrorBoundary": false, "module": "/assets/root-BT9N8_XD.js", "imports": ["/assets/index-DFWxKLZT.js", "/assets/components-DuNu-YZ6.js"], "css": ["/assets/root-C_nZ8B_o.css"] }, "routes/api.keystatic.$": { "id": "routes/api.keystatic.$", "parentId": "root", "path": "api/keystatic/*", "index": void 0, "caseSensitive": void 0, "hasAction": true, "hasLoader": true, "hasClientAction": false, "hasClientLoader": false, "hasErrorBoundary": false, "module": "/assets/api.keystatic._-l0sNRNKZ.js", "imports": [], "css": [] }, "routes/keystatic.$": { "id": "routes/keystatic.$", "parentId": "root", "path": "keystatic/*", "index": void 0, "caseSensitive": void 0, "hasAction": false, "hasLoader": false, "hasClientAction": false, "hasClientLoader": false, "hasErrorBoundary": false, "module": "/assets/keystatic._-uaeISFt4.js", "imports": ["/assets/index-DFWxKLZT.js", "/assets/index-1h9o7eJh.js"], "css": [] }, "routes/posts.$slug": { "id": "routes/posts.$slug", "parentId": "root", "path": "posts/:slug", "index": void 0, "caseSensitive": void 0, "hasAction": false, "hasLoader": true, "hasClientAction": false, "hasClientLoader": false, "hasErrorBoundary": false, "module": "/assets/posts._slug-DM5BKevZ.js", "imports": ["/assets/index-DFWxKLZT.js", "/assets/index-1h9o7eJh.js", "/assets/components-DuNu-YZ6.js"], "css": [] }, "routes/_index": { "id": "routes/_index", "parentId": "root", "path": void 0, "index": true, "caseSensitive": void 0, "hasAction": false, "hasLoader": true, "hasClientAction": false, "hasClientLoader": false, "hasErrorBoundary": false, "module": "/assets/_index-DnK16LYq.js", "imports": ["/assets/index-DFWxKLZT.js", "/assets/components-DuNu-YZ6.js"], "css": [] } }, "url": "/assets/manifest-d2aca31e.js", "version": "d2aca31e" };
+const serverManifest = { "entry": { "module": "/assets/entry.client-CfmtkiP9.js", "imports": ["/assets/index-DFWxKLZT.js", "/assets/components-DuNu-YZ6.js"], "css": [] }, "routes": { "root": { "id": "root", "parentId": void 0, "path": "", "index": void 0, "caseSensitive": void 0, "hasAction": false, "hasLoader": false, "hasClientAction": false, "hasClientLoader": false, "hasErrorBoundary": false, "module": "/assets/root-BT9N8_XD.js", "imports": ["/assets/index-DFWxKLZT.js", "/assets/components-DuNu-YZ6.js"], "css": ["/assets/root-C_nZ8B_o.css"] }, "routes/api.keystatic.$": { "id": "routes/api.keystatic.$", "parentId": "root", "path": "api/keystatic/*", "index": void 0, "caseSensitive": void 0, "hasAction": true, "hasLoader": true, "hasClientAction": false, "hasClientLoader": false, "hasErrorBoundary": false, "module": "/assets/api.keystatic._-l0sNRNKZ.js", "imports": [], "css": [] }, "routes/keystatic.$": { "id": "routes/keystatic.$", "parentId": "root", "path": "keystatic/*", "index": void 0, "caseSensitive": void 0, "hasAction": false, "hasLoader": false, "hasClientAction": false, "hasClientLoader": false, "hasErrorBoundary": false, "module": "/assets/keystatic._-DmfnRtUD.js", "imports": ["/assets/index-DFWxKLZT.js", "/assets/index-1h9o7eJh.js"], "css": [] }, "routes/posts.$slug": { "id": "routes/posts.$slug", "parentId": "root", "path": "posts/:slug", "index": void 0, "caseSensitive": void 0, "hasAction": false, "hasLoader": true, "hasClientAction": false, "hasClientLoader": false, "hasErrorBoundary": false, "module": "/assets/posts._slug-DM5BKevZ.js", "imports": ["/assets/index-DFWxKLZT.js", "/assets/index-1h9o7eJh.js", "/assets/components-DuNu-YZ6.js"], "css": [] }, "routes/_index": { "id": "routes/_index", "parentId": "root", "path": void 0, "index": true, "caseSensitive": void 0, "hasAction": false, "hasLoader": true, "hasClientAction": false, "hasClientLoader": false, "hasErrorBoundary": false, "module": "/assets/_index-DnK16LYq.js", "imports": ["/assets/index-DFWxKLZT.js", "/assets/components-DuNu-YZ6.js"], "css": [] } }, "url": "/assets/manifest-73f4f98e.js", "version": "73f4f98e" };
 const mode = "production";
 const assetsBuildDirectory = "build/client";
 const basename = "/";

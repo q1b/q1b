99178151ba2dcb9c7d36e64ca768cae91e7fca8f
Sukhpreet Singh <sukhpreetben10@gmail.com>
2024-09-19 15:49:59 +0530

    Updated admin vite.config.js

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,9 +1,16 @@
 import { vitePlugin as remix } from '@remix-run/dev';
 import { defineConfig } from 'vite';
+import { vercelPreset } from "@vercel/remix/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { installGlobals } from "@remix-run/node";
+
+installGlobals();
 
 export default defineConfig({
-  plugins: [remix()],
-  server: {
-    host: '127.0.0.1',
-  },
+	plugins: [remix({
+		presets: [vercelPreset(), tsconfigPaths()]
+	})],
+	server: {
+		host: '127.0.0.1',
+	},
 });

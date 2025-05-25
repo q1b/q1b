85e944688a1925dcfc75f3e911db3f35d239dae6
q1b <sukhpreetben10@gmail.com>
2025-05-25 11:49:14 +0530

    added right bottom edit icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
-
 import cloudflare from "@astrojs/cloudflare";
-
 import sitemap from "@astrojs/sitemap";
-
 import markdoc from "@astrojs/markdoc";
 
 // https://astro.build/config
